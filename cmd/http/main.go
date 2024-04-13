29851dcb86029e513682a054ea7b4b13684ea2de
Kazuenori Ono(wallon) <matopenkw@gmail.com>
2024-04-13 16:11:51 +0900

    add: echo2 handler

--- cmd/http/main.go
+++ cmd/http/main.go
@@ -33,9 +33,24 @@ func echoHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Echo!")
 }
 
+func echo2Handler(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path != "/echo" {
+		http.Error(w, "404 not found.", http.StatusNotFound)
+		return
+	}
+
+	if r.Method != "GET" {
+		http.Error(w, "Method is not supported.", http.StatusNotFound)
+		return
+	}
+
+	fmt.Fprintf(w, "Echo2!")
+}
+
 func main() {
 	http.HandleFunc("/hello", helloHandler)
 	http.HandleFunc("/echo", echoHandler)
+	http.HandleFunc("/echo2", echo2Handler)
 
 	fmt.Printf("Starting server at port 8080\n")
 	if err := http.ListenAndServe(":8080", nil); err != nil {
