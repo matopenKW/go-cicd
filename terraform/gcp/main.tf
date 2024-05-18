3435cd93eacfee42a97fbe095b1af7d74259992e
Kazunori Ono <matopenkw@gmail.com>
2024-05-18 16:36:21 +0900

    wip

--- terraform/gcp/main.tf
+++ terraform/gcp/main.tf
@@ -13,6 +13,11 @@ terraform {
 resource "google_project" "go_cicd" {
   name            = "go-cicd"
   project_id      = "wallon-go-cicd"
+  lifecycle {
+    ignore_changes = [
+      billing_account
+    ]
+  }
 }
 
 
