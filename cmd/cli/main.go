b5ffc4ed7dfba6a82ca1e54012517727520dea62
Kazunori Ono <matopenkw@gmail.com>
2024-05-19 00:40:41 +0900

    wip

---
+++ cmd/cli/main.go
@@ -0,0 +1,8 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("test cli")
+	fmt.Println("Hello, World!")
+}
--- terraform/gcp/main.tf
+++ terraform/gcp/main.tf
@@ -28,6 +28,12 @@ resource "google_project_service" "artifact_registry" {
   disable_on_destroy = false
 }
 
+resource "google_project_service" "iam_googleapis_com" {
+  project = google_project.go_cicd.project_id
+  service = "iam.googleapis.com"
+
+  disable_on_destroy = false
+}
 
 #resource "google_project_service" "secret_manager" {
 #  project = google_project.go_cicd.project_id
